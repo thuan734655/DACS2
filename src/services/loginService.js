4af1aac8caeab655449cc39d618faf5d8a64caef
hapdev05 <demoover81@gmail.com>
2024-11-28 16:18:16 +0700

    fix port

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:7749";
+const BASE_URL = "http://localhost:5000";
 
 export const requestOTP = async (email) => {
   return await axios.post(`${BASE_URL}/requestOTP`, { email });
