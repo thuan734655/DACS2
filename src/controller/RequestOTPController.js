025a8d202e2dccceb03c0f430ec39d42e7a2b77b
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-12-12 20:24:51 +0700

    ok 2FA quen mat khau

--- src/controller/RequestOTPController.js
+++ src/controller/RequestOTPController.js
@@ -1,17 +1,17 @@
 import { requestOTP } from "../services/loginService";
 
 export const HandleRequestOtp = async (e, email, setLoading, setStep) => {
-    e.preventDefault();
-    setLoading(true);
-    try {
-      const response = await requestOTP(email);
-      alert(response.data.message);
-      if (typeof setStep === "function") {
-        setStep(2);
-      }
-    } catch (error) {
-      alert(error.response?.data?.message || "Request failed.");
-    } finally {
-      setLoading(false);
+  e.preventDefault();
+  setLoading(true);
+  try {
+    const response = await requestOTP(email);
+    alert(response.data.message);
+    if (typeof setStep === "function") {
+      setStep(2);
     }
-  };
+  } catch (error) {
+    alert(error.response?.data?.message || "Request failed.");
+  } finally {
+    setLoading(false);
+  }
+};
